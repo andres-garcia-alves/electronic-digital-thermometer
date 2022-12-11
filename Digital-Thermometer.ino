61a5c8c71e4dc022d0c2926c7e6fab4b0be9936a
Andres Garcia Alves <andres_garcia_ao@hotmail.com>
2022-12-10 23:26:31 -0300

    Re-arrange LEDs pin-out to simplefy PCB

--- Digital-Thermometer.ino
+++ Digital-Thermometer.ino
@@ -1,29 +1,71 @@
+/*
+Digital Thermometer v1.1
+
+Created by: Andres Garcia Alves <andres.garcia.alves@gmail.com>
+Microcontroller: Arduino Nano (ATMega328 @16Mhz)
+
+Version 1.0, 2022.12.08 - Initial release.
+Version 1.1, 2022.12.10 - Re-arrange LEDs pin-out to simplefy PCB.
+
+This source code is licensed under GPL v3.0
+
+Pinout (4 digit multiplexed 7-segment display):
+
+     ***(A)***
+  (F)*       *(B)
+     ***(G)***
+  (E)*       *(C)
+     ***(D)***
+
+D00 -> debug (serial port)
+D01 -> debug (serial port)
+D02 -> output to F segment of selected digit
+D03 -> output to A segment of selected digit
+D04 -> output to selector pin for digit 0
+D05 -> output to E segment of selected digit
+D06 -> output to D segment of selected digit
+D07 -> output to dot point of selected digit
+D08 -> output to C segment of selected digit
+D09 -> output to G segment of selected digit
+D10 -> output to selector pin for digit 1
+D11 -> output to B segment of selected digit
+D12 -> output to selector pin for digit 2
+D13 -> output to selector pin for digit 3
+
+*/
+
 #include <DHT.h>
 
-// 4 digit multiplexed 7-segment display
-
-#define DEBUG         true
+#define DEBUG         false
 
 // digit selector pins
 #define PIN_DIGIT_0   4
-#define PIN_DIGIT_1   3
-#define PIN_DIGIT_2   2
-#define PIN_DIGIT_3   5
+#define PIN_DIGIT_1   13
+#define PIN_DIGIT_2   12
+#define PIN_DIGIT_3   10
 
 // display segment pins
-#define PIN_LED_A     11
-#define PIN_LED_B     10
-#define PIN_LED_C     9
-#define PIN_LED_D     7
-#define PIN_LED_E     6
-#define PIN_LED_F     12
-#define PIN_LED_G     8
-#define PIN_LED_DOT   13
+#define PIN_LED_A     3
+#define PIN_LED_B     11
+#define PIN_LED_C     8
+#define PIN_LED_D     6
+#define PIN_LED_E     5
+#define PIN_LED_F     2
+#define PIN_LED_G     9
+#define PIN_LED_DOT   7
 
-#define PIN_DHT       A0      // DHT sensor pin
-#define SENSOR_TYPE   DHT11   // sensor type
+// DHT sensor pin & sensor type
+#define PIN_DHT       A4
+#define SENSOR_TYPE   DHT11
 
 // data structures
+struct SensorMeasure {
+  bool valid = false;
+  float temperature = 0;
+  float humidity = 0;
+  float heatIndex = 0;
+} sensorMeasure;
+
 struct DisplayText {
   String temperature;
   String humidity;
@@ -31,7 +73,7 @@ struct DisplayText {
   byte temperatureDotPos;
   byte humidityDotPos;
   byte heatIndexDotPos;
-};
+} displayText;
 
 enum eDataMode { NONE, TEMPERATURE, HUMIDITY, HEAT_INDEX };
 
@@ -43,16 +85,8 @@ unsigned long int currentMillis = 0;
 unsigned long int nextSensorTick = 0;
 unsigned long int nextDataModeTick = 0;
 
-float temperature = 0;
-float humidity = 0;
-float heatIndex = 0;
-
 eDataMode dataMode = NONE;
-DisplayText displayText;
 byte currentDigit = 0;
-char currentChar = ' ';
-bool dotEnabled = false;
-
 
 // initial setup
 void setup() {
@@ -61,10 +95,11 @@ void setup() {
   #if DEBUG
   Serial.begin(9600);
   #endif
-  
-  // iniciar sensor DHT
+
+  // inicializar sensor DHT
   dht.begin();
 
+  // modo de los pines
   pinMode(PIN_DIGIT_0, OUTPUT);
   pinMode(PIN_DIGIT_1, OUTPUT);
   pinMode(PIN_DIGIT_2, OUTPUT);
@@ -78,6 +113,8 @@ void setup() {
   pinMode(PIN_LED_F, OUTPUT);
   pinMode(PIN_LED_G, OUTPUT);
   pinMode(PIN_LED_DOT, OUTPUT);
+
+  delay(300);
 }
 
 
@@ -88,15 +125,17 @@ void loop() {
 
   // sensor reading
   if (currentMillis > nextSensorTick) {
-    nextSensorTick = currentMillis + 10000; // next sensor read in 10 secods
+    nextSensorTick = currentMillis + 15000; // next sensor read in 15 secods
 
-    bool valid = readSensor();
-    if (valid) {
-      #if DEBUG
-      updateSerialPort();
-      #endif
-      displayText = buildDisplayText();
-    }
+    sensorMeasure = readSensor();
+
+    if (sensorMeasure.valid) {
+      displayText = buildDisplayText(sensorMeasure);
+    }      
+
+    #if DEBUG
+    updateSerialPort(sensorMeasure);
+    #endif
   }
 
   // data mode
@@ -110,57 +149,68 @@ void loop() {
       case HEAT_INDEX:  dataMode = TEMPERATURE; break;
     }
   }
-  
-  // update display
-  updateDisplay();
+
+  // update display & move to next digit
+  if (sensorMeasure.valid) {
+    updateDisplay(dataMode, currentDigit);
+    currentDigit = (++currentDigit) % 4; // valid range 0-3
+  }
+
   delay(5);
 }
 
 
-bool readSensor() {
-  temperature = dht.readTemperature();  // temperatura (en grados centígrados)
-  humidity = dht.readHumidity();        // humedad relativa
+SensorMeasure readSensor() {
+
+  SensorMeasure aux;
+  aux.valid = true;
+  aux.temperature = dht.readTemperature();  // temperatura (en grados centígrados)
+  aux.humidity = dht.readHumidity();        // humedad relativa
 
   // comprobar que no haya habido algún error en la lectura
-  if (isnan(temperature) || isnan(humidity)) {
+  if (isnan(aux.temperature) || isnan(aux.humidity)) {
+    aux.valid = false;
+
     #if DEBUG
     Serial.println("Error obteniendo los datos del sensor DHT11");
     #endif
-    return false;
   }
 
   // calcular la sensación térmica (en grados centígrados)
-  heatIndex = dht.computeHeatIndex(temperature, humidity, false);
-  return true;
+  aux.heatIndex = dht.computeHeatIndex(aux.temperature, aux.humidity, false);
+
+  return aux;
 }
 
 
-DisplayText buildDisplayText() {
-  DisplayText displayText;
+DisplayText buildDisplayText(SensorMeasure sensorMeasure) {
+
+  DisplayText aux;
+  aux.temperature = String(sensorMeasure.temperature, 1);
+  aux.temperature.replace(".", "");
+  aux.temperature += "c";
+  aux.temperatureDotPos = 1;
   
-  displayText.temperature = String(temperature, 1);
-  displayText.temperature.replace(".", "");
-  displayText.temperature += "c";
-  displayText.temperatureDotPos = 1;
+  aux.humidity = String(sensorMeasure.humidity, 1);
+  aux.humidity.replace(".", "");
+  aux.humidity.setCharAt(2, " ");
+  aux.humidity += "h";
+  aux.humidityDotPos = -1;
   
-  displayText.humidity = String(humidity, 1);
-  displayText.humidity.replace(".", "");
-  displayText.humidity.setCharAt(2, " ");
-  displayText.humidity += "h";
-  displayText.humidityDotPos = -1;
-  
-  displayText.heatIndex = String(heatIndex, 1);
-  displayText.heatIndex.replace(".", "");
-  displayText.heatIndex += "C";  
-  displayText.heatIndexDotPos = 1;
- 
-  return displayText;
+  aux.heatIndex = String(sensorMeasure.heatIndex, 1);
+  aux.heatIndex.replace(".", "");
+  aux.heatIndex += "C";  
+  aux.heatIndexDotPos = 1;
+
+  return aux;
 }
 
 
-void updateDisplay() {
+void updateDisplay(eDataMode dataMode, byte currentDigit) {
+  char character = ' ';
+  bool dotEnabled = false;
 
-  // enable display current digit
+  // enable current digit
   digitalWrite(PIN_DIGIT_0, (currentDigit == 0) ? HIGH : LOW);
   digitalWrite(PIN_DIGIT_1, (currentDigit == 1) ? HIGH : LOW);
   digitalWrite(PIN_DIGIT_2, (currentDigit == 2) ? HIGH : LOW);
@@ -168,25 +218,24 @@ void updateDisplay() {
 
   // retrieve character for the current digit
   if (dataMode == TEMPERATURE) {
-    currentChar = displayText.temperature.charAt(currentDigit);
+    character = displayText.temperature.charAt(currentDigit);
     dotEnabled = (displayText.temperatureDotPos == currentDigit) ? true : false;
   } else if (dataMode == HUMIDITY) {
-    currentChar = displayText.humidity.charAt(currentDigit);
+    character = displayText.humidity.charAt(currentDigit);
     dotEnabled = (displayText.humidityDotPos == currentDigit) ? true : false;
   } else if (dataMode == HEAT_INDEX) {
-    currentChar = displayText.heatIndex.charAt(currentDigit);
+    character = displayText.heatIndex.charAt(currentDigit);
     dotEnabled = (displayText.heatIndexDotPos == currentDigit) ? true : false;
   }
 
   // display character
-  displayCharacter(currentChar, dotEnabled);
-
-  // move to next digit
-  currentDigit = (++currentDigit) % 4; // valid range 0-3
+  displayCharacter(character, dotEnabled);
 }
 
 
 void displayCharacter(char character, bool dotEnabled) {
+
+  digitalWrite(PIN_LED_DOT, !dotEnabled);
 
   if (character == '0') {
     digitalWrite(PIN_LED_A, LOW);
@@ -301,21 +350,20 @@ void displayCharacter(char character, bool dotEnabled) {
     digitalWrite(PIN_LED_F, HIGH);
     digitalWrite(PIN_LED_G, HIGH);
   }
-
-  digitalWrite(PIN_LED_DOT, !dotEnabled);
 }
 
 
 #if DEBUG
-void updateSerialPort() {
+void updateSerialPort(SensorMeasure sensorMeasure) {
+
   Serial.print("Temperatura: ");
-  Serial.print(temperature);
+  Serial.print(sensorMeasure.temperature);
   Serial.print(" ºC \t");
   Serial.print("Humedad: ");
-  Serial.print(humidity);
+  Serial.print(sensorMeasure.humidity);
   Serial.print(" % \t");
   Serial.print("Sensación térmica: ");
-  Serial.print(heatIndex);
+  Serial.print(sensorMeasure.heatIndex);
   Serial.println(" ºC");
 }
 #endif
